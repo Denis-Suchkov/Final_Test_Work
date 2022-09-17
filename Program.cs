3ba2a713059d86c261dc69b2f9cc73d386f92dd5
Denis <Suchkov_81@mail.ru>
2022-09-17 23:58:22 +0300

    add GetResArray

--- Program.cs
+++ Program.cs
@@ -8,5 +8,23 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+Console.Clear();
+string[] array = new string[11] {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
+string[] res = new string[array.Length];
 
+GetResArray(array, res);
+PrintArray(res);
+
+void GetResArray(string[] array, string[] res)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        res[count] = array[i];
+        count++;
+        }
+    }
+}
 
